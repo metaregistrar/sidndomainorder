13a73e426ae8d419ee26f5bcacb521fd466e754b
Ewout de Graaf <ewout@metaregistrar.com>
2025-04-24 10:37:26 +0200

    Added comments about username and password to config.php

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
 // Contains EPP username and password. DO NOT PUSH TO GIT!!!!
+// EPP username for SIDN contains 6 numbers. It is your login ID without DSU or ADM
 CONST EPPUSERNAME = '';
+
+// EPP password cannot exceed 16 characters
 CONST EPPPASSWORD = '';
